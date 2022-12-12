691f23f7f0673da4ac540c968218a9ca0fa754b4
Norberto Luna <betoluna89@gmail.com>
2022-12-12 12:47:06 -0300

    Comienzo Firebase

--- src/index.js
+++ src/index.js
@@ -9,6 +9,19 @@ import { router } from "./router";
 // Context
 import { CartContextProvider } from "./context/cartContext";
 
+// FIREBASE
+import { initializeApp } from "firebase/app";
+const firebaseConfig = {
+  apiKey: "AIzaSyBzsx1eNhxAlkvnMqR8feC6ONHN31W1tko",
+  authDomain: "ecommerce-alabama.firebaseapp.com",
+  projectId: "ecommerce-alabama",
+  storageBucket: "ecommerce-alabama.appspot.com",
+  messagingSenderId: "960842863067",
+  appId: "1:960842863067:web:115f7f6f2bef894dd297fe"
+};
+initializeApp(firebaseConfig);
+
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
